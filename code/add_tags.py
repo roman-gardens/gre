fe9f1fe08a2ed2974637ea72d4b4cf7051313e11
Christian Casey <christiancasey86@gmail.com>
2021-04-12 14:02:12 +0200

    Tweaking python script to add tags, about to make changes to files

--- code/add_tags.py
+++ code/add_tags.py
@@ -14,71 +14,78 @@ sBaseDir = '../content/province'
 
 # Navigate the directory tree start at a predefined root directory
 for sRoot, vDirs, vFiles in os.walk(sBaseDir):
-    
-    # Loop through all the files in the current directory
-    for sFilename in vFiles:
-        _, sExt = os.path.splitext(sFilename)   # Get file extension (looking for .md)
-        
-        # Work with only .md files, but not with any _index.md files
-        # This leaves only the entries proper
-        if sExt == '.md' and not sFilename == '_index.md':
-            
-            print(sFilename)
-            
-            with open(os.path.join(sRoot,sFilename),'r') as f:
-                sFile = f.read()
-                f.close()
-            
-            # Get the keyword section of the file
-            sKeywords = sFile
-            iStart = sKeywords.find('## Keywords')
-            sKeywords = sFile[iStart+12:]
-            iEnd = sKeywords.find('#')
-            sKeywords = sKeywords[:iEnd].strip()
-            
-            # Extract a list of keywords
-            vKeywords = []
-            for tMatch in re.findall(reKeywordEntries, sKeywords):
-                if not tMatch in vKeywords:
-                    sKeyword = tMatch[0]
-                    sKeyword = re.sub('\([^\)]*\)', '', sKeyword)
-                    sKeyword = sKeyword.strip()
-                    if len(sKeyword):
-                        vKeywords.append(sKeyword)
-            
-            # Skip the file if it does not contain any keywords
-            if not len(vKeywords):
-                print('File does not contain keywords: %s' % sFilename)
-                continue
-            
-            # Compile the list into a new 'tags' entry
-            vKeywords.sort()
-            
-            # Get the metadata section of the file
-            sMetaData = sFile
-            iEnd = sMetaData.find('---\n', 5)
-            if iEnd == -1:
-                raise Error('File does not contain Metadata: %s' % sFilename)
-            
-            sMetaData = sMetaData[:iEnd+4]
-            sMetaData = re.sub('\-\-\-\n', '', sMetaData)
-            sMetaData = sMetaData.strip()
-            
-            sFile = sFile[iEnd+4:].strip()
-            
-            # Extract any 'tags' section already in the metadata
-            iTags = sMetaData.find('tags:')
-            if iTags > -1:
-                sMetaData = sMetaData[:iTags].strip()
-            
-            # Create MetaData tag list
-            sTags = 'tags:\n'
-            for sKeyword in vKeywords:
-                sTags += ' - "%s"\n' % sKeyword
-                print(sKeyword)
-                
-            sMetaData = '---\n%s\n%s---\n\n' % (sMetaData, sTags)
-            
-            with open(os.path.join(sRoot,sFilename),'w') as f:
-                f.write(sMetaData + sFile)
-                f.close()
+	
+	# Loop through all the files in the current directory
+	for sFilename in vFiles:
+		_, sExt = os.path.splitext(sFilename)   # Get file extension (looking for .md)
+		
+		# Work with only .md files, but not with any _index.md files
+		# This leaves only the entries proper
+		if sExt == '.md' and not sFilename == '_index.md':
+			
+			print(sFilename)
+			
+			with open(os.path.join(sRoot,sFilename),'r') as f:
+				sFile = f.read()
+				f.close()
+			
+			reDraft = re.compile('draft\:\s+[Tt]rue')
+			bSearch = reDraft.search(sFile)
+			print(bSearch)
+			
+			# Only change things if the entry is NOT a draft
+			if bSearch is None:
+				
+				# Get the keyword section of the file
+				sKeywords = sFile
+				iStart = sKeywords.find('## Keywords')
+				sKeywords = sFile[iStart+12:]
+				iEnd = sKeywords.find('#')
+				sKeywords = sKeywords[:iEnd].strip()
+				
+				# Extract a list of keywords
+				vKeywords = []
+				for tMatch in re.findall(reKeywordEntries, sKeywords):
+					if not tMatch in vKeywords:
+						sKeyword = tMatch[0]
+						sKeyword = re.sub('\([^\)]*\)', '', sKeyword)
+						sKeyword = sKeyword.strip()
+						if len(sKeyword):
+							vKeywords.append(sKeyword)
+				
+				# Skip the file if it does not contain any keywords
+				if not len(vKeywords):
+					print('File does not contain keywords: %s' % sFilename)
+					continue
+				
+				# Compile the list into a new 'tags' entry
+				vKeywords.sort()
+				
+				# Get the metadata section of the file
+				sMetaData = sFile
+				iEnd = sMetaData.find('---\n', 5)
+				if iEnd == -1:
+					raise Error('File does not contain Metadata: %s' % sFilename)
+				
+				sMetaData = sMetaData[:iEnd+4]
+				sMetaData = re.sub('\-\-\-\n', '', sMetaData)
+				sMetaData = sMetaData.strip()
+				
+				sFile = sFile[iEnd+4:].strip()
+				
+				# Extract any 'tags' section already in the metadata
+				iTags = sMetaData.find('tags:')
+				if iTags > -1:
+					sMetaData = sMetaData[:iTags].strip()
+				
+				# Create MetaData tag list
+				sTags = 'tags:\n'
+				for sKeyword in vKeywords:
+					sTags += ' - "%s"\n' % sKeyword
+					print(sKeyword)
+					
+				sMetaData = '---\n%s\n%s---\n\n' % (sMetaData, sTags)
+				
+				with open(os.path.join(sRoot,sFilename),'w') as f:
+					# f.write(sMetaData + sFile)
+					f.close()
